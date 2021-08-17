47926cfd7b24262b28017d0e2d032388c2c9d0b1
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-17 22:06:13 +0200

    set up handlebars templets

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const path = require("path");
-
-const members = require("./Members");
+const exphbs = require("express-handlebars");
+const members = require('./Members')
 const logger = require("./middleware/logger");
 
 const app = express();
 
-app.use(logger);
+// app.use(logger);
 
-app.get("/api/members", (req, res) => res.json(members));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index', {
+  title: 'member App',
+  members
+}))
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 // app.get("/", (req, res) => {
 //   res.send(path.join(__dirname, 'public', 'index.html'));
